11c089d0fc5ac92d3e3095fa5ce86c19fbd89798
Hasireddy <malikireddy.uma@gmail.com>
2026-02-23 09:57:18 +0100

    Update book method

--- app.py
+++ app.py
@@ -30,5 +30,43 @@ def handle_books():
 
 
 
+
+
+def find_book_by_id(book_id):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+    return None
+
+
+@app.route("/api/books/<int:id>", methods=['GET', 'PUT'])
+def update_book(id):
+    if request.method == 'PUT':
+        book_to_update  = find_book_by_id(id)
+
+        if book_to_update is None:
+            return jsonify({"error": "Book not found"}), 404
+
+        new_data = request.get_json()
+
+        if not new_data or not validate_book(new_data):
+            return jsonify({"error": "Invalid book data"}), 400
+
+        book_to_update["title"] = new_data["title"]
+        book_to_update["author"]= new_data["author"]
+
+        return jsonify(book_to_update), 200
+
+    else:
+        #If GET request display book with given id
+        book = find_book_by_id(id)
+        if book is None:
+            return jsonify({"error": "Book not found"}), 404
+
+        return jsonify(book), 200
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
