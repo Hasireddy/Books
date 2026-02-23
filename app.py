551bf06aa79409fee051ec050b63c17b85b5e274
Hasireddy <malikireddy.uma@gmail.com>
2026-02-23 08:59:20 +0100

    Add new book api

--- app.py
+++ app.py
@@ -15,3 +15,20 @@ def validate_book(data):
 
 @app.route("/api/books", methods=['GET', 'POST'])
 def handle_books():
+    if request.method == 'POST':
+        new_book = request.get_json()
+        if not validate_book(new_book):
+            return jsonify({"error": "Invalid book data"}), 400
+
+        new_id = max(book['id'] for book in books)+1
+        new_book["id"] = new_id
+        books.append(new_book)
+        return jsonify(new_book), 201
+
+    else:
+        return jsonify(books), 200
+
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
